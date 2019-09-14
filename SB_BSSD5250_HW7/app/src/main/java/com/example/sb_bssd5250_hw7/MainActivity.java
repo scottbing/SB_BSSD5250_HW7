3e2fc7c492afa2a4d8922b659478d7ee1e2ba7fa
scottbing <sbing52@comcast.net>
2019-09-14 10:22:40 -0600

    SB_BSSD5250_HW7
    SB_BSSD5250_HW7

---
+++ SB_BSSD5250_HW7/app/src/main/java/com/example/sb_bssd5250_hw7/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.sb_bssd5250_hw7;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity  implements NotesData.NotesDataUpdatedListener {
+
+    private RecyclerView notesRV;
+    private int i = 1;
+
+
+    @Override
+    public void updateNotesDependents() {
+        NotesAdapter notesAdapter = new NotesAdapter();
+        notesAdapter.setmContext(this);
+        notesRV.swapAdapter(notesAdapter, true);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)	{
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        // setup an array of notes
+        ArrayList<Note> notesArray = NotesData.getInstance(this).getNoteList();
+
+        setupViewport();
+
+    }
+
+    private void setupViewport() {
+
+        NotesData.getInstance(this).setListener(this);
+
+        notesRV	= new RecyclerView(  this);
+        notesRV.setBackgroundColor(Color.RED);
+
+        NotesAdapter notesAdapter = new NotesAdapter();
+        notesAdapter.setmContext(this);
+        notesRV.setAdapter(notesAdapter);
+        notesRV.setLayoutManager(new LinearLayoutManager( this));
+
+        Button addNoteButton = new Button( this);
+        addNoteButton.setText("+");
+        addNoteButton.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,  ViewGroup.LayoutParams.WRAP_CONTENT));
+        //make this button add a new note when clicked
+        addNoteButton.setOnClickListener(addClickedListener);
+
+        LinearLayout linearLayout = new LinearLayout( this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,  ViewGroup.LayoutParams.MATCH_PARENT));
+        linearLayout.addView(addNoteButton);
+        linearLayout.addView(notesRV);
+
+        setContentView(linearLayout);
+
+    }
+
+    private View.OnClickListener addClickedListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.d("addClick Listener", "add clicked");
+            Note note = new Note();
+            //note.setName("Note New");
+            //note.setDesc("Desc New");
+            note.setName("Note New " + String.valueOf(i));
+            note.setDesc("Desc New " + String.valueOf(i++));
+            NotesData.getInstance(MainActivity.this).getNoteList().add(note);
+            setupViewport();
+
+        }
+    };
+}
---
+++ SB_BSSD5250_HW7/app/src/main/java/com/example/sb_bssd5250_hw7/NoteEditorDialog.java
@@ -0,0 +1,90 @@
+package com.example.sb_bssd5250_hw7;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.AlertDialog;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import org.w3c.dom.Text;
+
+public class NoteEditorDialog extends DialogFragment {
+
+    public static final String EXTRA_NAME = "com.example.sb_bssd5250_hw7a";
+    public static final String EXTRA_DATE = "com.example.sb_bssd5250_hw7b";
+    public static final String EXTRA_DESC = "com.example.sb_bssd5250_hw7c";
+
+    private EditText nameText;
+    private EditText dateText;
+    private EditText descText;
+
+    private int position;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        //return super.onCreateDialog(savedInstanceState);
+
+        position = getArguments().getInt( "position");
+        Note note = NotesData.getInstance(null).getNoteList().get(position);
+
+        nameText = new EditText(getActivity());
+        //nameText.setHint("Enter Name");
+        nameText.setText(note.getName());
+        dateText = new EditText(getActivity());
+        //dateText.setHint("Enter Date");
+        dateText.setText(note.getDate());
+        descText = new EditText(getActivity());
+        //descText.setHint("Enter Description");
+        descText.setText(note.getDesc());
+
+        // this is taken from: https://stackoverflow.com/questions/12876624/multiple-edittext-objects-in-alertdialog
+        LinearLayout alertLayout = new LinearLayout(getContext());
+        alertLayout.setOrientation(LinearLayout.VERTICAL);
+        alertLayout.setId(View.generateViewId());
+
+        alertLayout.addView(nameText);
+        alertLayout.addView(dateText);
+        alertLayout.addView(descText);
+
+        return new AlertDialog.Builder(getActivity())
+                .setTitle("Modify Note")
+                .setView(alertLayout)
+                .setPositiveButton("Done", doneClickedListener)
+                .setNegativeButton("Cancel", null)
+                .create();
+
+    }
+
+    private void sendResult(int resultCode) {
+        if (getTargetFragment() == null) {
+            // Target not set up, do nothing
+        } else {
+            Intent i = new Intent();
+            i.putExtra( EXTRA_NAME, nameText.getText().toString());
+            i.putExtra( EXTRA_DATE, dateText.getText().toString());
+            i.putExtra( EXTRA_DESC, descText.getText().toString());
+            getTargetFragment().onActivityResult(getTargetRequestCode(), resultCode, i);
+        }
+    }
+
+    private DialogInterface.OnClickListener doneClickedListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+            //Log.d("noteEditorDialog", nameText.getText().toString());
+            NotesData.getInstance(null).getNoteList().get(position).setName(nameText.getText().toString());
+            NotesData.getInstance(null).getNoteList().get(position).setDate(dateText.getText().toString());
+            NotesData.getInstance(null).getNoteList().get(position).setDesc(descText.getText().toString());
+            NotesData.getInstance(null).refreshNotes();
+        }
+    };
+}
